4002ca991e9d1f32ea8e1e7997a104c911682ab7
Brandon Kelley <14bmkelley@gmail.com>
2016-03-04 18:18:56 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  
+  int svr_socket, req_socket;
+  int buffer_size = 1024;
+  char *buffer = malloc(buffsize);
+  struct sockaddr_in svr_addr;
+  socklen_t svr_addr_len;
+  int result;
+  
+  svr_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (svr_socket > 0) {
+    printf("The socket was created.\n");
+  }
+  
+  svr_addr.sin_family = AF_INET;
+  svr_addr.sin_addr.s_addr = INADDR_ANY;
+  svr_addr.sin_port = htons(8080);
+  svr_addr_len = sizeof(svr_addr);
+  
+  result = bind(svr_socket, (struct sockaddr *) &svr_addr, svr_addr_len);
+  if (result == 0) {
+    printf("Binding socket.\n");
+  }
+  
+  while(1) {
+    
+    result = listen(svr_socket, 10);
+    if (result < 0) {
+      perror("Server: listen");
+      exit(1);
+    }
+    
+    req_socket = accept(svr_socket, (struct sockaddr *) &svr_addr, &svr_addr_len);
+    if (req_socket < 0) {
+      perror("Server: accept");
+      exit(1);
+    }
+    
+    if (req_socket > 0) {
+      printf("A client connected...\n");
+    }
+    
+    do {
+      result = recv(req_socket, buffer, buffer_size, 0);
+    } while (result != 0);
+    
+    printf("Recieved: %s\n", buffer);
+    
+    // Write a response
+    write(req_socket, "HTTP/1.1 200 OK\n", 16);
+    write(req_socket, "Content-length: 46\n", 19);
+    write(req_socket, "Content-Type: text/html\n\n", 25);
+    write(req_socket, "<html><body><h1>Hello world</h1></body></html>", 46);
+    
+    close(req_socket);
+    
+  }
+  
+  close(svr_socket);
+  return 0;
+  
+}
+
